9d1d42be8ee742fa1ba4da3e9303b4516d540fe2
Abhi Chakraborty <abhichakraborty@Abhis-MacBook-Air.local>
2021-11-20 23:44:57 +0530

    Version Two Build Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,11 @@ import Avatar from "@material-ui/core/Avatar";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
 import ArrowBack from "@material-ui/icons/ArrowBack";
-import AssignmentInd from "@material-ui/icons/AssignmentInd";
+
 import ScatterPlotIcon from '@material-ui/icons/ScatterPlot';
 import AdjustIcon from '@material-ui/icons/Adjust';
 import TimelineIcon from '@material-ui/icons/Timeline';
 import Home from "@material-ui/icons/Home";
-import Apps from "@material-ui/icons/Apps";
-import ContactMail from "@material-ui/icons/ContactMail";
 import { makeStyles } from "@material-ui/core/styles";
 import avatar from "../avatar.png";
 
